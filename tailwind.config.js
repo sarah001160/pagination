c340c0821e4f651af03c5482d0b6fe4f99e88f16
sarah001160 <sarah001160@gmail.com>
2024-12-08 15:55:09 +0800

    變更檔名index.html、調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"], //這行的意思是 Tailwind CSS 會在 src 資料夾中的所有 HTML 和 JavaScript 檔案中，搜尋所有用到的 CSS 類名，並將這些類名保留在最終生成的 CSS 檔案中。
+  content: ["./src/**/*.{html,js}", "./index.html"], //這行的意思是 Tailwind CSS 會在 src 資料夾中的所有 HTML 和 JavaScript 檔案中，搜尋所有用到的 CSS 類名，並將這些類名保留在最終生成的 CSS 檔案中。
   theme: {
     extend: {
       colors: {
